eafd96eabfd7ff847cc20e316404810dd5d8317b
Marcus Krahl <mail@marcuskrahl.de>
2015-10-13 15:47:58 +0200

    Added URL to HTMLMatchPlanRetriever

--- app/src/main/java/de/marcuskrahl/dynamospielplan/HtmlMatchPlanRetriever.java
+++ app/src/main/java/de/marcuskrahl/dynamospielplan/HtmlMatchPlanRetriever.java
@@ -1,6 +1,17 @@
 package de.marcuskrahl.dynamospielplan;
 
+import java.net.URL;
+
 public class HtmlMatchPlanRetriever {
+
+    private URL url;
+
+    public HtmlMatchPlanRetriever(URL url) {
+        if (url == null) {
+            throw new NullPointerException();
+        }
+        this.url = url;
+    }
 
     public MatchPlan retrieve() {
         return new MatchPlan();
--- app/src/test/java/de/marcuskrahl/dynamospielplan/test/HtmlMatchPlanRetrieverTest.java
+++ app/src/test/java/de/marcuskrahl/dynamospielplan/test/HtmlMatchPlanRetrieverTest.java
@@ -5,13 +5,21 @@ import org.junit.*;
 import de.marcuskrahl.dynamospielplan.HtmlMatchPlanRetriever;
 import de.marcuskrahl.dynamospielplan.MatchPlan;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static org.junit.Assert.*;
 
 public class HtmlMatchPlanRetrieverTest {
 
+    @Test(expected = NullPointerException.class)
+    public void WhenCalledWithoutURL_HtmlMatchPlanRetriever_ThrowsNullPointerException() {
+        HtmlMatchPlanRetriever retriever = new HtmlMatchPlanRetriever(null);
+    }
+
     @Test
-    public void WhenCalled_HtmlMatchPlanRetriever_ReturnsMatchPlan() {
-        HtmlMatchPlanRetriever retriever = new HtmlMatchPlanRetriever();
+    public void WhenCalled_HtmlMatchPlanRetriever_ReturnsMatchPlan() throws MalformedURLException {
+        HtmlMatchPlanRetriever retriever = new HtmlMatchPlanRetriever(new URL("http://www.example.com"));
 
         MatchPlan plan = retriever.retrieve();
 
